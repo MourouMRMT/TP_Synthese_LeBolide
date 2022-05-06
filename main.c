05b0c6d96db7a81b099f2c1cf34fafa264502e58
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2022-05-06 20:14:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 /******************************************************************************/
                 /*Program by MARIMOUTOU Mourougen*/
                             /*Date:04/2022*/
-                            /*Revision:0.2*/
+                            /*Revision:0.3*/
 /******************************************************************************/
-
-
 /******************************************************************************/
 /* Files to Include                                                           */
 /******************************************************************************/
@@ -19,32 +17,33 @@
 
 #if defined(__XC) || defined(HI_TECH_C)
 
+
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
 #include <stdio.h>
 #include<stdlib.h>
 #include <string.h>
 #include <pic18f45k50.h>
-
+//#include"C:\Users\mouro\Desktop\CoursGE\TP_de_Synthese\Code\SSD1306OLED.c"
 
 #endif
 
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"       /* User funct/params, such as InitApp */
 
-#define _XTAL_FREQ 4000000 //Fixe la frequence de l'oscilateur sur 4MHz
+#define _XTAL_FREQ 4000000
 
 #pragma config FOSC=INTOSCIO
 #pragma config WDTEN=OFF
 #pragma config MCLRE=OFF
 #pragma config LVP=OFF
 
+
+
 /*
 Config:
- -D0,D1:moteur arriere gauche
- -D2,D3:moteur arriere droit
- -D4,D5:moteur avant gauche
- -D6,D7:moteur avant droit
+ -D4,D5:moteur avant gauche,moteur arriere gauche
+ -D6,D7:moteur avant droit,moteur arriere droit
  D0,D2,D4,D6:marche avant
  D1,D3,D5,D7:marche arriere
 
@@ -55,27 +54,23 @@ Config:
  * Capteur sonore:
  -Pin Trigger:RE0
  */
-
 /******************************************************************************/
 /* User Global Variable Declaration                                           */
 /******************************************************************************/
-
+int imp=0;
 /* i.e. uint8_t <variable_name>; */
-
 /******************************************************************************/
-/*                          Sous Programme                                    */
+/* Main Program                                                               */
 /******************************************************************************/
-
 
 void clap()
 {
     if(PORTEbits.RE0==1)
     {
-        LATBbits.LATB7=1;
-    }else{LATBbits.LATB7=0;}
-
+        //LATBbits.LATB7=1;
+        imp=1;
+    }//else{LATBbits.LATB7=0;}
 }
-
 
 void avance(int imp)
 {
@@ -85,7 +80,6 @@ void avance(int imp)
         LATDbits.LATD1=0;
         LATDbits.LATD2=1;
         LATDbits.LATD3=0;
-
     }else{LATD=0;}
 }
 void recule(int imp)
@@ -99,13 +93,8 @@ void recule(int imp)
 
     }else{LATD=0;}
 }
-
 void tourner_droite()
 {
-    LATDbits.LATD0=0;
-    LATDbits.LATD1=1;
-    LATDbits.LATD2=1;
-    LATDbits.LATD3=0;
     LATDbits.LATD4=0;
     LATDbits.LATD5=1;
     LATDbits.LATD6=1;
@@ -116,20 +105,18 @@ void tourner_droite()
 void tourner_gauche()
 {
 
-    LATDbits.LATD0=1;
-    LATDbits.LATD1=0;
-    LATDbits.LATD2=0;
-    LATDbits.LATD3=1;
     LATDbits.LATD4=1;
     LATDbits.LATD5=0;
     LATDbits.LATD6=0;
     LATDbits.LATD7=1;
 }
 
+
 void arret()
 {
     LATD=0;
 }
+
 
 void detection_obstacle()
 {
@@ -137,32 +124,39 @@ void detection_obstacle()
     LATCbits.LATC0=1;
     __delay_us(10);
     LATCbits.LATC0=0;
+
     //Temporisation afin de determiner la duree
     while(PORTCbits.RC1==0);
-    T1CONbits.TMR1ON=1; //On debute le comptage
+    T1CONbits.TMR1ON=1;     //On debute le comptage
     while(PORTCbits.RC1==1);
-    T1CONbits.TMR1ON=0; //On arrete le comptage
+    T1CONbits.TMR1ON=0;     //On arrete le comptage
 
-    int Timer=TMR1; //Temps en µs
+
+    int Timer=TMR1;          //Temps en µs
     int Duree=Timer;
     int Distance;
-    if(Duree<120) //Valeur aleatoire qui marche bien ^^
+    if(Duree<180)
     {
         Distance=Duree/58 ;
-        arret();
-    }else{avance(1);}
+        imp=0;
+    }//else{avance(imp);}
     Duree=0;
     TMR1=0;
 
 }
 
+
 void affichage()
 {
+  /*SSD1306_Begin(SSD1306_SWITCHCAPVCC, 0X7A);
+  SSD1306_ClearDisplay();
+  SSD1306_SetTextWrap(false);
+  SSD1306_GotoXY(25, 0);
+  SSD1306_Print("Hello world");
+  SSD1306_Display();*/
 }
 
-/******************************************************************************/
-/*                             Main Program                                   */
-/******************************************************************************/
+
 void main(void)
 {
     /* Configure the oscillator for the device */
@@ -173,20 +167,28 @@ void main(void)
 
     /* TODO <INSERT USER APPLICATION CODE HERE> */
     TRISD=0;
-    ANSELD=0;
-    TRISCbits.TRISC1=1;
-    TRISCbits.TRISC0=0;
     ANSELC=0;
-    int i;
+    ANSELB=0;
+    ANSELD=0;
+    ANSELE=0;
+    TRISCbits.TRISC1=1;
+    TRISEbits.TRISE0=1;
+    TRISCbits.TRISC0=0;
+    TRISBbits.TRISB7=0;
+
     while(1)
     {
-        //clap();
-        //detection_obstacle();
+        clap();
+        //arret();
+        //affichage();
+
         //LATD0=!LATD0;
-        //avance(1);
+        avance(imp);
+        detection_obstacle();
         //__delay_ms(1000);
         //recule(1);
-        //__delay_ms(5000);
+        //__delay_ms(1000);
+
         //for(i=0;i<1024;i++);
     }
 
