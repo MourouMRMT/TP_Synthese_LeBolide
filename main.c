d0559763521eba6464f72b7b6534b00d2f32fba5
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2022-05-29 15:03:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /******************************************************************************/
                 /*Program by MARIMOUTOU Mourougen*/
                             /*Date:04/2022*/
-                            /*Revision:0.4*/
+                            /*Revision:0.5*/
 /******************************************************************************/
 /******************************************************************************/
 /* Files to Include                                                           */
@@ -24,6 +24,11 @@
 #include<stdlib.h>
 #include <string.h>
 #include <pic18f45k50.h>
+
+
+
+
+
 //#include"C:\Users\mouro\Desktop\CoursGE\TP_de_Synthese\Code\SSD1306OLED.c"
 
 #endif
@@ -31,13 +36,13 @@
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"       /* User funct/params, such as InitApp */
 
+#include "C:\Users\mouro\Desktop\CoursGE\TP_de_Synthese\Code\I2C_LCD.h"
 #define _XTAL_FREQ 4000000
 
 #pragma config FOSC=INTOSCIO
 #pragma config WDTEN=OFF
 #pragma config MCLRE=OFF
 #pragma config LVP=OFF
-
 
 
 /*
@@ -49,7 +54,7 @@ Config:
 
  * Capteur a ultrasons:
  * Mur:
- -pin Echo: RC1
+ -pin Echo: RC4
  -Pin Trigger:RC0
  *Boite:
  -pin Echo: RC7
@@ -57,22 +62,45 @@ Config:
 
  * Capteur sonore:
  -Pin Trigger:RE0
+ *
+ * Sharp Sensor:
+ * Analog read :RAO
  */
 /******************************************************************************/
 /* User Global Variable Declaration                                           */
 /******************************************************************************/
-int imp=0,obs=0;
+int imp=0,obs=0,distance=0;
 int cpt=0;
 /* i.e. uint8_t <variable_name>; */
 /******************************************************************************/
 /* Main Program                                                               */
 /******************************************************************************/
+void ADC_init()
+{
+    ADCON0=0;
+    ADCON1=0;
+    ADCON2=0;
+}
+
+int ADC_Read(int channel)
+{
+    int digit;
+    ADCON0=(ADCON0 & 0b11000011)|((channel<<2) & 0b00111100);
+    ADCON0|=((1<<ADON)|(1<<GO));
+
+    while(ADCON0bits.GO_nDONE==1);
+    digit=(ADRESH*256)|(ADRESL);
+
+    return digit;
+
+}
 
 void clap()
 {
     if(PORTEbits.RE0==1)
     {
         imp=1;
+
     }
 }
 void avance(int imp)
@@ -85,14 +113,20 @@ void avance(int imp)
         LATDbits.LATD3=0;
     }else{LATD=0;}
 }
+
+void avance_PWM(int var)
+{
+    LATDbits.LATD4=var;
+    LATDbits.LATD5=var;
+}
 void recule(int imp)
 {
     if(imp==1)
     {
-        LATDbits.LATD0=0;
-        LATDbits.LATD1=1;
-        LATDbits.LATD2=0;
-        LATDbits.LATD3=1;
+        LATDbits.LATD0=1;
+        LATDbits.LATD1=0;
+        LATDbits.LATD2=1;
+        LATDbits.LATD3=0;
 
     }else{LATD=0;}
 }
@@ -100,10 +134,10 @@ void tourner_droite()
 {
     if(imp==1)
     {
-    LATDbits.LATD0=0;
-    LATDbits.LATD1=1;
-    LATDbits.LATD2=1;
-    LATDbits.LATD3=0;
+    LATDbits.LATD0=1;
+    LATDbits.LATD1=0;
+    LATDbits.LATD2=0;
+    LATDbits.LATD3=1;
     }
 
 }
@@ -113,10 +147,10 @@ void tourner_gauche()
     if(imp==1)
     {
 
-    LATDbits.LATD0=1;
-    LATDbits.LATD1=0;
-    LATDbits.LATD2=0;
-    LATDbits.LATD3=1;
+    LATDbits.LATD0=0;
+    LATDbits.LATD1=1;
+    LATDbits.LATD2=1;
+    LATDbits.LATD3=0;
     }
 }
 
@@ -126,7 +160,22 @@ void arret()
     LATD=0;
 }
 
+void Sharp_detector()
+{
+  float range=0; // value from sensor * (5/1024)
+  int digit = ADC_Read(0); // worked out from datasheet graph
+  range = digit*((float)5.0/(float)1024.0);
+  if (range> 30){
+      LATEbits.LATE1=1;
 
+  }else
+  {
+      LATEbits.LATE1=0;
+      imp=0;// stop the bolide
+  }
+
+
+}
 void detection_obstacle()
 {
     //Generarion MLI
@@ -135,9 +184,9 @@ void detection_obstacle()
     LATCbits.LATC0=0;
 
     //Temporisation afin de determiner la duree
-    while(PORTCbits.RC1==0);
+    while(PORTCbits.RC4==0);
     T1CONbits.TMR1ON=1;     //On debute le comptage
-    while(PORTCbits.RC1==1);
+    while(PORTCbits.RC4==1);
     T1CONbits.TMR1ON=0;     //On arrete le comptage
 
 
@@ -179,11 +228,12 @@ void tour_boite()
     {
         imp=0;
         LATEbits.LATE1=0;
+        tourner_gauche();
         //Distance=Duree2/58 ;
 
     }else
-    {tourner_gauche();
-        //avance(imp);
+    {
+        avance(imp);
         LATEbits.LATE1=1;
 
     }
@@ -194,12 +244,7 @@ void tour_boite()
 
 void affichage()
 {
-  /*SSD1306_Begin(SSD1306_SWITCHCAPVCC, 0X7A);
-  SSD1306_ClearDisplay();
-  SSD1306_SetTextWrap(false);
-  SSD1306_GotoXY(25, 0);
-  SSD1306_Print("Hello world");
-  SSD1306_Display();*/
+
 }
 
 
@@ -215,23 +260,42 @@ void main(void)
     TRISD=0;
     ANSELC=0;
     ANSELB=0;
-    ANSELD=0;
+
     ANSELE=0;
-    TRISCbits.TRISC1=1;
+    TRISCbits.TRISC4=1;
     TRISEbits.TRISE0=1;
-    TRISEbits.TRISE1=0;
+
     TRISCbits.TRISC0=0;
     TRISBbits.TRISB7=0;
     TRISCbits.TRISC7=0;
     TRISCbits.TRISC6=1;
 
+    ANSELDbits.ANSD0=0;
+    ANSELDbits.ANSD1=0;
+    ANSELDbits.ANSD2=0;
+    ANSELDbits.ANSD3=0;
+
+    ANSELDbits.ANSD4=1;
+    ANSELDbits.ANSD5=1;
+    //Sharp Sensor
+    TRISAbits.RA0=1;
+    ANSELAbits.ANSA0=1;
+    TRISEbits.TRISE1=0;
+    //Afficheur
+    //PWM
+    ANSELCbits.ANSC2=1;
+    TRISCbits.TRISC2=0;
     while(1)
     {
         clap();
+        avance(imp);
+        avance_PWM(255);
+        Sharp_detector();
         //tour_boite();
         //detection_obstacle();
         //tourner_gauche();
         //affichage();
+        //avance_PWM(120);
 
     }
 
